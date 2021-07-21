f0c598dd248affa84105ff1844af6682643033dd
zeppelinux <dmitry@diligesoft.com>
2021-07-20 18:13:56 -0700

    add logs

--- src/main/java/com/diligesoft/qddns/checks/LivenessCheck.java
+++ src/main/java/com/diligesoft/qddns/checks/LivenessCheck.java
@@ -5,6 +5,7 @@ import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
 import org.eclipse.microprofile.health.Liveness;
+import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
--- src/main/java/com/diligesoft/qddns/checks/ReadinessCheck.java
+++ src/main/java/com/diligesoft/qddns/checks/ReadinessCheck.java
@@ -5,6 +5,7 @@ import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
 import org.eclipse.microprofile.health.Readiness;
+import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -16,10 +17,15 @@ public class ReadinessCheck implements HealthCheck {
     @Inject
     AmazonRoute53 client;
 
+
+    @Inject
+    Logger logger;
+
     HealthCheckResponseBuilder responseBuilder = HealthCheckResponse.named("Readiness check");
 
     @Override
     public HealthCheckResponse call() {
+        logger.debug("in readinessCheck, Amazon client is " + (client == null?"null":"initialized"));
         return (client != null?responseBuilder.up():responseBuilder.down()).build();
     }
 }
